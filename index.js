f9f1431b575aa6aeac4cfe150061aaf1a97b7d9b
michaelberish <mikeyberish@gmail.com>
2022-04-07 14:50:31 -0700

    done

--- index.js
+++ index.js
@@ -14,3 +14,29 @@
 //add if statement for inputBox
 
 //everything is stored in the current browser
+
+function pageLoad() {
+    const fruit = localStorage.getItem("fruit")
+    const inputBox = localStorage.getItem('inputBox')
+
+    if (fruit) {
+        document.getElementById('product').value = fruit
+    }
+    document.getElementById('product').addEventListener('change', afterChange)
+    if (inputBox) {
+        document.getElementById('inputBox').value = inputBox
+    }
+    document.getElementById('inputBox').addEventListener('input', afterInput)
+}
+
+function afterChange(e) {
+    console.log(e.target.value);
+    localStorage.setItem('fruit', e.target.value)
+}
+
+function afterInput(e) {
+    console.log(e.target.value)
+    localStorage.setItem('inputBox', e.target.value)
+}
+
+document.addEventListener('DOMContentLoaded', pageLoad)
