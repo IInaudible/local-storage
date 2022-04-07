6637e88987949882b380a56aff069ae02c055ba5
halowe-cfa <91778081+halowe-cfa@users.noreply.github.com>
2022-04-07 13:32:00 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+//create empty pageLoad function
+//add DOMContentLoadedEventListener
+//inside of pageLoad, add event listener to product id
+//then create afterChange function with console log
+//check in console then add local storage
+//add getItem to pageLoad function
+//set getItem equal to fruit
+//console.log fruit
+//add if statement to set the value of fruit in pageLoad function
+//this will keep the value of the previous selected value
+//create event for input box in pageLoad
+//add afterInput function
+//creat inputBox variable to get Item from local storage
+//add if statement for inputBox
+
+//everything is stored in the current browser
